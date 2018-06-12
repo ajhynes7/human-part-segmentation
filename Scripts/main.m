44f2d1d83e73b095e8107e45dfd81bdf433cfb0d
ajhynes7 <38727640+ajhynes7@users.noreply.github.com>
2018-06-12 11:13:22 -0400

    Added Scripts folder

---
+++ Scripts/main.m
@@ -0,0 +1,133 @@
+
+clc, clearvars, close all;
+
+%-------------------------------------------------------------------------
+% Human Part Segmentation in Depth Images with Annotated Part Positions
+%
+% Andrew Hynes
+% June 11, 2018
+%-------------------------------------------------------------------------
+
+path_to_data = '../Input Data/Human Limbs from RGBD Data';
+
+addpath('../Functions/Graphs')
+addpath('../Functions/Results')
+addpath('../Functions/General')
+addpath('../Functions/Clustering')
+addpath('../Functions/Segmentation')
+addpath('../Functions/MATLAB File Exchange')
+addpath('../Functions/GraphAnalysisToolbox (by Leo Grady)')
+
+
+%% Parameters
+
+dataset = 'n';
+
+% If set to True, only the pure random walker algorithm will be run,
+% i.e. no layered graph will be constructed
+pure_RW = false;
+
+% Joint connections (e.g., torso [1] to RU arm [2])
+joint_conn = [1 2; 2 3; 3 4; 1 5; 5 6; 6 7];
+
+% Draw a line of seed pixels for this joint connection
+is_line = [1 0 0 1 0 0];
+
+% Ratios for labelling the lines of seed pixels
+ratios = [2/3, nan, nan, 2/3, nan, nan, nan, nan, nan];
+
+% Numbers of the arm parts, first column is the hands
+% Row 1 is right, 2 is left
+arm_parts  = [4 3; 7 6];
+
+% Beta values for Random Walker weighting function
+% For arm segmentation and final segmentation
+betas = [90, 90];
+
+% Radius for mean shift with Gaussian kernel (standard deviation)
+% Mean shift used to segment arm
+r = 0.01;
+
+
+
+%% Load data
+
+load_filename = sprintf('hernandez_clean_%s', dataset);
+
+load_path = fullfile(path_to_data, load_filename);
+load(load_path)
+
+n_frames = numel(depth_cell);
+
+
+%% Process frames
+
+[part_seg_cell, arm_seg_cell] = deal(cell(n_frames, 1));
+
+for f = 14
+        
+    depth        = depth_cell{f};
+    truth        = truth_cell{f};
+    image_coords = image_coords_cell{f};
+    
+    % Segment body parts
+    if pure_RW == true
+        
+        % Segment with pure Random Walker (no layered graph)
+        part_seg = segment_parts_pure(depth, image_coords, joint_conn,...
+            is_line, betas, ratios);
+    else
+        
+        % Segment with layered graph
+        [part_seg, arm_seg, connect_pix, is_valid] = ...
+            segment_parts(depth, image_coords, joint_conn, is_line,...
+            betas, arm_parts, ratios, r);
+        
+        arm_seg_cell{f} = arm_seg;
+    end
+    
+    part_seg_cell{f} = part_seg;
+end
+
+
+
+%% Numerical results
+
+% Create confusion matrix
+C = multi_class_confusion(part_seg, truth);
+
+% Per-class accuracy and Jaccard index
+[~, PC, JI] = confusion_matrix_results(C);
+
+fprintf('Mean per-class accuracy: \n%.4f \n\n', nanmean(PC));
+fprintf('Mean Jaccard index:      \n%.4f \n\n', nanmean(JI));
+
+
+
+%% Visual results
+
+background = 'w';
+n_parts = size(image_coords, 1);
+colours = distinguishable_colors(n_parts, background);
+
+if pure_RW == false
+     
+    figure;
+    RGB = label2rgb(arm_seg, colours, background);
+    imagesc(RGB);
+    title(sprintf('Arm segmentation: Set %s - Frame %d', dataset, f))
+    
+    fprintf('Valid arms: \n%d %d \n\n', is_valid(1), is_valid(2)); 
+end
+
+figure;
+RGB = label2rgb(part_seg, colours, background);
+imagesc(RGB);
+title(sprintf('Part segmentation: Set %s - Frame %d', dataset, f))
+hold on;
+scatter(image_coords(:, 1), image_coords(:, 2), 'filled',...
+    'w', 'MarkerEdgeColor', 'k')
+
+
+
+
---
+++ Scripts/prepare_humanlimb_dataset.m
@@ -0,0 +1,66 @@
+clc, clearvars, close all;
+
+addpath('../Functions/General')
+
+dataset = 'n';
+
+data_dir = '../../../MEGA/Human limb mat files';
+
+load_filename = sprintf('hernandez_raw_%s.mat', dataset);
+save_filename = sprintf('hernandez_clean_%s.mat', dataset);
+
+load_path = fullfile(data_dir, load_filename);
+
+load(load_path)
+
+
+%% Process files
+
+% Numbers of all the arm parts: hand to shoulder
+% Row 1 is right, 2 is left
+arm_orders  = [4 3 2; 7 6 5];
+
+n_frames = size(depth_cell, 1);
+
+orig_depth_cell = depth_cell;
+orig_truth_cell = truth_cell;
+[depth_cell, truth_cell, image_coords_cell] = deal(cell(n_frames, 1));
+
+for f = 1:n_frames
+    
+    depth = orig_depth_cell{f};
+    truth = orig_truth_cell{f};
+    
+    % Change truth labels
+    truth = replace_by_vec(truth, [2, 16:18, 24:26], 1:7);
+    
+    % Delete high depths
+    delete_index = depth > 2500;
+    depth(delete_index) = nan;
+    truth(delete_index) = 0;
+    
+    image_coords = pixels_near_centroids(truth);
+     
+    % Change torso coordinate
+    R_shoulder = image_coords(arm_orders(1, end), :);
+    L_shoulder = image_coords(arm_orders(2, end), :);
+    
+    mid_of_shoulders = round(mean([L_shoulder; R_shoulder]));
+    
+    if ~any(isnan(mid_of_shoulders))
+        image_coords(1, :) = mid_of_shoulders;
+    end
+    
+    % Store data in cells
+    depth_cell{f} = depth;
+    truth_cell{f} = truth;
+    image_coords_cell{f} = image_coords;
+end
+
+
+%% Save processed data
+
+save_path = fullfile('../Input Data/Human Limbs from RGBD Data', save_filename);
+
+save(save_path, 'depth_cell', 'truth_cell', 'image_coords_cell', '-v7.3')
+
